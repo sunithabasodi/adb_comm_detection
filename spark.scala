c5e11637d1ee7b7b96754a05a3aab98dc32ff2f8
Ankush Chauhan <ankush.26.11@gmail.com>
2016-11-03 00:01:10 -0400

    SNAP Facebook Loader in Python

--- spark.scala
+++ spark.scala
@@ -1,2 +1,13 @@
-val textFile = sc.textFile("Dataset/readme-Ego.txt")
-textFile.count()
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
+val fb_dir="Dataset\facebook"
+val circles = sc.textFile(fb_dir+"\\"+"0"+".circles")
+val circlesRDD
+
+case class spark.pycircles(circleID: String, nodeIDs: Seq[Int])
+case class edges(to: Int, from: Int)
+case class feat(nodeID: Int, features: Seq[Int])
+case class egofeat(egoID: Int, features: Seq[Int])
+case class featnames(featureID: Int, features_names:Seq[String])
+
+case class egoNet [egoNetID, circles:circles,edges:edges,feat:feat,egofeat:egofeat,featnames:featnames]
